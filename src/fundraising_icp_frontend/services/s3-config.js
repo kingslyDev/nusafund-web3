1de99e379dbb232d19d6d0e3eaa8b8159c3e08ac
kingslydev <ghalyallcoc@gmail.com>
2024-12-14 18:14:00 +0700

    Remove s3-config.js from Git tracking

--- src/fundraising_icp_frontend/services/s3-config.js
+++
@@ -1,9 +0,0 @@
-import AWS from 'aws-sdk';
-
-const s3 = new AWS.S3({
-  accessKeyId: 'AKIAUW4RASVDS3E62E5S', // Ganti dengan Access Key ID Anda
-  secretAccessKey: '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', // Ganti dengan Secret Access Key Anda
-  region: 'ap-southeast-1', // Ganti dengan region bucket Anda
-});
-
-export default s3;
